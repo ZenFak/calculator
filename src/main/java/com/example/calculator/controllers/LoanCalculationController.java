51f615282fe608dfdefe37ea8f806f4317678ed1
ZenFak <andrkat200@gmail.com>
2024-06-03 19:24:55 +0300

    Add Swagger documentation

--- src/main/java/com/example/calculator/controllers/LoanCalculationController.java
+++ src/main/java/com/example/calculator/controllers/LoanCalculationController.java
@@ -5,6 +5,8 @@ import com.example.calculator.dto.LoanOfferDto;
 import com.example.calculator.dto.LoanStatementRequestDto;
 import com.example.calculator.dto.ScoringDataDto;
 import com.example.calculator.loanCalculationService.LoanCalculationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -18,17 +20,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/calculator")
+@Tag(name = "LoanCalculation API", description = "APIs for loan calculation")
 public class LoanCalculationController {
     @Autowired
     private LoanCalculationService loanCalculationService;
 
     @PostMapping("/offers")
+    @Operation(summary = "Calculate possible loan offers",
+            description = "Calculates possible loan offers based on the loan statement request data.")
     public ResponseEntity<List<LoanOfferDto>> calculateLoanOffers(@RequestBody @Validated LoanStatementRequestDto request) {
         List<LoanOfferDto> loanOffers = loanCalculationService.calculateLoanOffers(request);
         return ResponseEntity.ok(loanOffers);
     }
 
     @PostMapping("/calc")
+    @Operation(summary = "Validate and calculate full loan parameters",
+            description = "Validates the scoring data and calculates full loan parameters including monthly payments and payment schedule.")
     public ResponseEntity<CreditDto> calculateCredit(@RequestBody @Validated ScoringDataDto scoringData) {
         CreditDto creditDto = loanCalculationService.calculateCredit(scoringData);
         return ResponseEntity.ok(creditDto);
